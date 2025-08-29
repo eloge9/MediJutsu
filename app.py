deb638821a3711b80b990326bfe2ff277c113ad5
Knig2 <gominaeloge@gmail.com>
2025-08-29 00:24:24 +0000

    1234

--- app.py
+++ app.py
@@ -3993,7 +3993,9 @@ def liste_patients_dossier_medical():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
+
 
 
 # Afficher toutes les ressources
